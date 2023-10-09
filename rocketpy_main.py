51d6131bb0892b2cc1191b38a5c1794ed395a600
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2023-10-09 17:03:58 -0400

    Definition of rocketpy main class

---
+++ rocketpy_main.py
@@ -0,0 +1,27 @@
+from rocketpy import Environment, SolidMotor, Rocket, Flight
+
+
+"""
+
+DOCUMENTATION RESOURCES:
+
+Generic page
+https://docs.rocketpy.org/en/latest/notebooks/getting_started.html
+
+On how to use the Enviornment class
+https://docs.rocketpy.org/en/latest/notebooks/environment/environment_class_usage.html
+
+On how to use the SolidMotor class
+https://docs.rocketpy.org/en/latest/user/motors/solidmotor.html
+
+On how to use the Rocket class
+https://docs.rocketpy.org/en/latest/user/rocket.html
+
+Notes on position & coordinate systems
+https://docs.rocketpy.org/en/latest/user/positions.html
+
+On how to define a parachute
+https://docs.rocketpy.org/en/latest/user/rocket.html#adding-parachutes
+
+
+"""
